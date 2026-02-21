4dcfb559a6445fb9e61e469f5bc844809ea90df1
Andronic Rares-Constantin <rares-constantin.andronic@student.tuiasi.ro>
2026-02-22 00:34:48 +0200

    am filtrat toate numerele din continutul fisierului

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ with open('input.txt', "r") as f:
 semnePunctuatie = ['"', ';', ':', '.', '?', '!', ',', '\'', '-', '[', ']', '(', ')']
 rezultat = ""
 for caracter in text:
-    if caracter not in semnePunctuatie:
+    if caracter not in semnePunctuatie and not caracter.isdigit():
         rezultat += caracter
