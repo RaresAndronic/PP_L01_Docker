bda44300ee2efd895c0bbdaabdc8eea90f8ad4ab
Andronic Rares-Constantin <rares-constantin.andronic@student.tuiasi.ro>
2026-02-22 00:31:13 +0200

    am elemintat semnele de punctuatie din continutul fisierului

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 with open('input.txt', "r") as f:
     text = f.read()
-
+semnePunctuatie = ['"', ';', ':', '.', '?', '!', ',', '\'', '-', '[', ']', '(', ')']
+rezultat = ""
+for caracter in text:
+    if caracter not in semnePunctuatie:
+        rezultat += caracter
